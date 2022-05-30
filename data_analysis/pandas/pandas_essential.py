fe1d629a3e4f3ad8389e3913605caf3625f84b30
jiru xu <66343787+jiru1997@users.noreply.github.com>
2022-05-29 22:08:20 -0700

    Update pandas_essential.py

--- data_analysis/pandas/pandas_essential.py
+++ data_analysis/pandas/pandas_essential.py
@@ -1,1 +1,24 @@
+#------------------------------------------------------------------------------------------------------------------------------------#
+#data input
+data = pandas.read_csv('path', skip_rows = 1)
+data.head()
+data.tail()
 
+#------------------------------------------------------------------------------------------------------------------------------------#
+#dataframe / series
+data['col'] / data.col / data[['col', 'col1']] 
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#method
+data.col.value_counts(ascending=True)
+data.col.sort_values(ascending=True)
+data.sort_values(by=[])
+data[data.col == ''] 
+data[data.col.str.contains('')] 
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#plot
+import matplotlib.pyplot as plt 
+%matplotlib inline
+
+data.col.value_counts().plot(kind='line', color='maroon', figsize=(1, 2));
