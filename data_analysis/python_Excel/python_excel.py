941064e84d86ae67be3d54dfb325943f6571f216
jiru xu <66343787+jiru1997@users.noreply.github.com>
2022-06-02 21:24:34 -0700

    Update python_excel.py

--- data_analysis/python_Excel/python_excel.py
+++ data_analysis/python_Excel/python_excel.py
@@ -1,1 +1,11 @@
+#------------------------------------------------------------------------------------------------------------------------------------#
+#pandas
+dafaframe_excel = pandas.read_excel('')
+dafaframe_csv = pandas.read_csv('', header=None)
+dafaframe_txt = pandas.read_csv('')
 
+dafaframe_csv.iloc[1, 2]
+dafaframe_csv.loc[dafaframe_csv['name'] == 'n']
+dafaframe_csv['newcol'] = dafaframe_csv['name'].apply(lambda x : x)
+
+file = value.to_excel('', index=None)
