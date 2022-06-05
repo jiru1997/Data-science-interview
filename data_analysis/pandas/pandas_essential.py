05c02546fa65319eb20a2dabfdc3c3a4833c17ba
jiru xu <66343787+jiru1997@users.noreply.github.com>
2022-06-04 22:56:10 -0700

    Update pandas_essential.py

--- data_analysis/pandas/pandas_essential.py
+++ data_analysis/pandas/pandas_essential.py
@@ -1,7 +1,7 @@
 #------------------------------------------------------------------------------------------------------------------------------------#
 #data input
 data = pandas.read_csv('path', 
-	skip_rows = 1,
+	skiprows = 1,
 	sep = "\t",
 	header = None,
 	names = ['col1', 'col2'])
@@ -15,6 +15,12 @@ data.columns
 data.index
 data.dtypes
 data.set_index('col1', inplace = True)
+data.reset_index()
+data.drop('col1', axis = 1)
+data.drop(1, axis = 0)
+data.mean(axis = 0) #列数量不变，行发生改变
+
+data.to_excel('', index = False)
 
 #------------------------------------------------------------------------------------------------------------------------------------#
 #dataframe / series
@@ -34,6 +40,9 @@ dataframe1['col1'] 	 #col
 dataframe1.loc[1:2]  #row
 dataframe1.loc[:, 'col1']
 
+dataframe1.iloc[x, y]
+dataframe1.iat[x, y]
+
 #------------------------------------------------------------------------------------------------------------------------------------#
 #data search
 dataframe1.loc['row1', 'col1']
@@ -50,13 +59,37 @@ dataframe1.loc[:, 'col3'] = dataframe1.apply(func, axis = 1)
 
 dataframe2 = dataframe1.assign(col4 = func)
 
+dataframe1['col4'] = ' '
+dataframe1[dataframe1['row1'] < 2, 'col'] = 1
+
 #------------------------------------------------------------------------------------------------------------------------------------#
 #method
-data.col.value_counts(ascending=True)
-data.col.sort_values(ascending=True)
-data.sort_values(by=[])
-data[data.col == ''] 
-data[data.col.str.contains('')] 
+dataframe1.describe()
+dataframe1.sort_values(by = 'col1')
+dataframe1['col1'].max()
+dataframe1['col1'].unique()
+dataframe1['col1'].value_counts(ascending=True)
+dataframe1['col1'].sort_values()
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#null value
+dataframe1.isnull()
+dataframe1.notnull()
+
+dataframe1.dropna(axis = 'columns', how = 'all', inplace = True)
+dataframe1.dropna(axis = 'rows', how = 'all', inplace = True)
+
+dataframe1.fillna({'col1' : 0})
+dataframe1.loc[:, 'col1'] = dataframe1['col1'].fillna(0)
+dataframe1.loc[:, 'col1'] = dataframe1['col1'].fillna(method = 'ffill')
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#string series.str
+dataframe1['col1'].str.replace('a', '')
+
+str.len()
+str.startswith('')
+str.contains('')
 
 #------------------------------------------------------------------------------------------------------------------------------------#
 #plot
