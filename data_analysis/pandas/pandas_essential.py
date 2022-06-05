8899fb97573ed4f765283b899af25fce65a6da8d
jiru xu <66343787+jiru1997@users.noreply.github.com>
2022-06-04 18:33:44 -0700

    Update pandas_essential.py

--- data_analysis/pandas/pandas_essential.py
+++ data_analysis/pandas/pandas_essential.py
@@ -1,12 +1,54 @@
 #------------------------------------------------------------------------------------------------------------------------------------#
 #data input
-data = pandas.read_csv('path', skip_rows = 1)
+data = pandas.read_csv('path', 
+	skip_rows = 1,
+	sep = "\t",
+	header = None,
+	names = ['col1', 'col2'])
+
+data = pandas.read_excel('path')
+
 data.head()
 data.tail()
+data.shape
+data.columns
+data.index
+data.dtypes
+data.set_index('col1', inplace = True)
 
 #------------------------------------------------------------------------------------------------------------------------------------#
 #dataframe / series
-data['col'] / data.col / data[['col', 'col1']] 
+series1 = pandas.Series([1, 2]);
+series1 = pandas.Series([1, 2], index = ['row1', 'row2']);
+series1 = pandas.Series(dict);
+
+series1['row1']
+
+data = {
+	'col1' : [],
+	'col2' : []
+}
+dataframe1 = pandas.Dataframe(data)
+
+dataframe1['col1'] 	 #col
+dataframe1.loc[1:2]  #row
+dataframe1.loc[:, 'col1']
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#data search
+dataframe1.loc['row1', 'col1']
+dataframe1.loc['row1', ['col1', 'col2']]
+dataframe1.loc['row1':'row2', 'col1']
+dataframe1.loc[dataframe1['row1'] < 2, :] #返回符合条件的行的所有列
+dataframe1.loc[(dataframe1['row1'] < 2) & (dataframe1['row1'] > 3), :]
+dataframe1.loc[lambda rows: rows['row1'] > 1, :]
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#data modify
+dataframe1.loc[:, 'col3'] = dataframe1['col1']
+dataframe1.loc[:, 'col3'] = dataframe1.apply(func, axis = 1)
+
+dataframe2 = dataframe1.assign(col4 = func)
 
 #------------------------------------------------------------------------------------------------------------------------------------#
 #method
