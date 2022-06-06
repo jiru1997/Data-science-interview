c55b38cc2aac54ccd6152fd1eebb8ed9c1850ae0
jiru xu <66343787+jiru1997@users.noreply.github.com>
2022-06-06 14:59:36 -0700

    Update python_excel.py

--- data_analysis/python_Excel/python_excel.py
+++ data_analysis/python_Excel/python_excel.py
@@ -1,11 +1,37 @@
 #------------------------------------------------------------------------------------------------------------------------------------#
-#pandas
-dafaframe_excel = pandas.read_excel('')
-dafaframe_csv = pandas.read_csv('', header=None)
-dafaframe_txt = pandas.read_csv('')
+#workbook / sheet
+from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter, column_index_from_string
 
-dafaframe_csv.iloc[1, 2]
-dafaframe_csv.loc[dafaframe_csv['name'] == 'n']
-dafaframe_csv['newcol'] = dafaframe_csv['name'].apply(lambda x : x)
+wb = load_workbook('path')
+wb.sheetnames
 
-file = value.to_excel('', index=None)
+for sheet in wb:
+	print(sheet.title)
+
+sheet1 = wb.active
+sheet1 = wb['Sheet1']
+sheet1 = wb.get_sheet_by_name('Sheet1')
+sheet2 = wb.create_sheet('newsheet')
+
+sheet1.max_row / sheet1.max_column
+
+get_column_letter(2)
+column_index_from_string('A')
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#cell
+cell = sheet['A1']
+cell.row / cell.column / cell.value
+
+sheet.cell(1, 1) #first index 1
+
+#------------------------------------------------------------------------------------------------------------------------------------#
+#rows / columns
+col1 = sheet1['C']
+col1 = sheet1['B:C']
+col1[2].value
+
+
+
+wb.save('path')
